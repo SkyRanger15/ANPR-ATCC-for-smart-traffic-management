b24c8a1b3daeb0d0396c74bf49718b94a5e87765
Soumyajit Khan <quicksjit15@gmail.com>
2024-11-26 17:52:22 +0530

    multiple vehicle recognition

--- object_tracker/tracker.py
+++ object_tracker/tracker.py
@@ -23,6 +23,7 @@ class Tracker:
     def detect_frame(self,frame):
         results = self.model.track(frame, persist=True)[0]
         name_dict = results.names
+        valid_classes = ["car", "bus", "truck", "motorcycle", "bicycle"]
 
         dict = {}
         for box in results.boxes:
@@ -30,18 +31,33 @@ class Tracker:
             result = box.xyxy.tolist()[0]
             object_class_id = box.cls.tolist()[0]
             object_class_name = name_dict[object_class_id]
-            if object_class_name == "car" or object_class_name == "bus" or object_class_name == "truck" or object_class_name == "motorcycle" or object_class_name == "bicycle":
-                dict[track_id] = result
+            # if object_class_name == "car" or object_class_name == "bus" or object_class_name == "truck" or object_class_name == "motorcycle" or object_class_name == "bicycle":
+            #     dict[track_id] = result
+            if object_class_name in valid_classes:
+                dict[track_id] = {object_class_name: result}
+
+
 
         return dict
 
     def draw_annotations(self, frames, object_detections):
         output_video_frames = []
+        color_dict ={
+            "car": (0,255,0),
+            "bus": (0,0,255),
+            "truck": (255,0,0),
+            "motorcycle": (255,255,0),
+            "bicycle": (0,255,255)
+        }
         for frame, obj_detected in zip(frames, object_detections):
-            for track_id, bbox in obj_detected.items():
-                x1,y1,x2,y2 = bbox
+            for track_id, object_detail in obj_detected.items():
+                for obj_class, bbox in object_detail.items():
+                    object_color = color_dict[obj_class]
+                    x1,y1,x2,y2 = bbox
+                    label = f"{obj_class} {track_id}"
+                    cv2.putText(frame, label, (int(x1), int(y1 - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, object_color, 2)
 
-                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0,0,255), 2)
+                    cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), object_color, 2)
             output_video_frames.append(frame)
 
         return output_video_frames
