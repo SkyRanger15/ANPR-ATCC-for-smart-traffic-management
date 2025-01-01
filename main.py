12bb826565b28f44d2545f2daa30726ecf84441c
Soumyajit Khan <quicksjit15@gmail.com>
2025-01-01 21:11:17 +0530

    1st Jan 2025

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 from utils.video_utils import read_video, save_video, detect_vehicles
-from object_tracker.tracker import Tracker
+from object_tracker.tracker_3 import Tracker
 
 def main():
-
     frames = read_video("data/video.mp4")
 
-    #object tracking
-    obj_tracker = Tracker()
-    # result = obj_tracker.detect_objects(frames)
-
-    # output_frames = obj_tracker.draw_annotations(frames, result)
-    output_frames = obj_tracker.process_video(frames)
-
-
-    save_video(output_frames, "output/output2.avi")
+    track = Tracker()
+    result = track.process_video(frames)
 
 if __name__ == '__main__':
     main()
+
